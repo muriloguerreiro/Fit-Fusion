fe5706252b3a0049e26af16ef04713b4a611323a
murilo.badoco <guerreiro_murilo@hotmail.com>
2023-12-12 14:28:51 -0300

    fix: remove Dot Env and Axios dependencies

--- frontend/app/workouts/[id]/details/page.js
+++ frontend/app/workouts/[id]/details/page.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 import Image from 'next/image'
 import styles from './page.module.css'
 import ExerciseCard from './ExerciseCard'
--- frontend/app/workouts/[id]/page.js
+++ frontend/app/workouts/[id]/page.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 import styles from './page.module.css'
 
 
