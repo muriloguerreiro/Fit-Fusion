676a576b54e8644fa7c9942b471684046ad738a0
murilo.badoco <guerreiro_murilo@hotmail.com>
2024-01-04 04:21:50 -0300

    fix: Navigator Bug and JWT_SECRET

--- backend/src/controllers/loginController.js
+++ backend/src/controllers/loginController.js
@@ -2,8 +2,6 @@ const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const JWT_SECRET = process.env.JWT_SECRET
-
 const LoginController = {
     async login(req, res) {
         const { username, password } = req.body;
@@ -15,7 +13,7 @@ const LoginController = {
                 
                 const token = jwt.sign(
                     { userId: user.id },
-                    JWT_SECRET,
+                    process.env.JWT_SECRET,
                     { expiresIn: '2h' }
                 );
 
